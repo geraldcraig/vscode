d0f08381982270b1467c5e0893aa7fb1a1f50a8f
geraldcraig <geraldcraig@me.com>
2021-09-13 20:21:05 +0100

    updated

---
+++ configuredb.php
@@ -0,0 +1,30 @@
+<?PHP
+
+$user_name = "root";
+$password = "root";
+$database = "addressbook";
+$server = "localhost";
+
+$db_handle = mysqli_connect($server, $user_name, $password);
+
+print "Connection to the Server opened";
+
+print "Server found" . "<BR>";
+
+$database = "addressbook";
+
+$db_found = mysqli_select_db( $db_handle, $database );
+
+if ($db_found) {
+
+print "Database found";
+
+}
+
+else {
+
+print "Database not found";
+
+}
+
+?>
