ec6fbebe7b1a547675a0f519059856488bee8686
geraldcraig <geraldcraig@me.com>
2021-10-04 22:44:50 +0100

    login added

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php // login.php
+$host = 'localhost'; // Change as necessary
+$data = 'publications'; // Change as necessary
+$user = 'root'; // Change as necessary
+$pass = 'root'; // Change as necessary
+$chrs = 'utf8mb4';
+$attr = "mysql:host=$host;dbname=$data;charset=$chrs"; $opts =
+[
+PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES =>false,
+];
+?>
