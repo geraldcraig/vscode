0c61ca3fd82f1fab1473eecc0a0b2e50245864d5
geraldcraig <geraldcraig@me.com>
2021-11-14 08:49:42 +0000

    week 1 added

--- configuredb.php
+++ configuredb.php
@@ -11,7 +11,7 @@ print "Connection to the Server opened";
 
 print "Server found" . "<BR>";
 
-$database = "todo";
+$database = "hotels.com";
 
 $db_found = mysqli_select_db( $db_handle, $database );
 
@@ -26,5 +26,20 @@ else {
 print "Database not found";
 
 }
+
+$sql = "SELECT first_name FROM customer";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["customer_id"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. " ".
+    $row["email"]. " ". $row["password"]. " ". $row["phone_number"]. " ". $row["membership_number"]. 
+    " ". $row["country_id"]. " ". $row["payment_id"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 
 ?>
