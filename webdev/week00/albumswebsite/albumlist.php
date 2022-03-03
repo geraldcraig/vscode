8bad798b905ecb9f1a26ab022749351aa14e5f3f
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-03-03 17:17:19 +0000

    week00 updated

---
+++ webdev/week00/albumswebsite/albumlist.php
@@ -0,0 +1,87 @@
+<?php
+ 
+    $endpoint = "http://localhost/webdev/week00/albumsapi/api.php";
+ 
+    $result = file_get_contents($endpoint);
+ 
+    $data = json_decode($result, true);
+ 
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Album Website</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Homepage</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="albumlist.php">Top 500 <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<br>
+
+<div class="container">
+        <h1>Top 500 Albums</h1>
+        <table class="table striped">
+            <thead>
+                <tr>
+                    <th>Number</th>
+                    <th>Year</th>
+                    <th>Album</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach ($data as $row) {
+                   
+                        echo "<tr>
+                               <td>{$row['number']}</td>
+                               <td>{$row['year']}</td>
+                               <td>{$row['album']}</td>
+                           </tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+</body>
+</html>
