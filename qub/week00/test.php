20bde79137d3b2ff9927115c2215a6874aae6f17
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-03-15 08:41:39 +0000

    albumswebsite updated

---
+++ qub/week00/test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+    //PHP program to demonstrate the working of var_dump function  
+    $x = 25;  
+    //dump integer variable  
+    var_dump ($x);            
+    echo "</br>";  
+      
+    $y = 32.5;  
+    //dump float variable  
+    var_dump ($y);            
+    echo "</br>";  
+      
+    $bvalue = true;  
+    //dump boolean variable  
+    var_dump ($bvalue);  
+?>  
+
+</body>
+</html>
