7cc76f2a3d3b4bff8f5cb3587b92e796089d28b2
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-04-01 23:53:11 +0100

    crud added

--- studentstutorial.com/crud/backend/database.php
+++ studentstutorial.com/crud/backend/database.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "user";
+$dbname = "mydb";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
