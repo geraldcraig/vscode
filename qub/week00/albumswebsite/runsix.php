57ebeec79d72fc8f928f0c7e9456718ebca4f0b8
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-04-07 21:15:39 +0100

    updated

---
+++ qub/week00/albumswebsite/runsix.php
@@ -0,0 +1,38 @@
+<?php
+
+    include("dbconn.php");
+
+    $filename = "data/album_list.csv";
+
+    // get the resource object (open the file)
+    $contents = fopen($filename, "r");
+
+    // counter for output
+    $num_records = 0;
+
+    // loop to read each line from CSV file into $row array
+    while ( ($row = fgetcsv($contents)) !== FALSE ) {
+        
+        echo "<div>";
+        print_r($row);
+        echo "</div>";
+
+        // Perform insert queries
+        // 1 - album_image table: id (PK), album_id, image_id
+        $album_image_insert = "INSERT INTO album_image (album_id, image_id) VALUES ((SELECT id FROM album WHERE title = '$row[2]'),
+                  (SELECT id FROM image WHERE image = '$row[5]')) ";
+
+        $result = $conn -> query($album_image_insert);
+       
+            
+        if (!$result) {         
+           echo $conn -> error;       
+       } else {
+
+       $num_records++;
+   
+      } 
+    }
+
+    echo "<h3>Total of {$num_records} records inserted into database table</h3>";
+?>
