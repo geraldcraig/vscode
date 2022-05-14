c36306b316c54c4602a9616e0fcd67a14c68f9be
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-05-14 23:49:07 +0100

    api updated

--- qub/week00/albumsapi/api.php
+++ qub/week00/albumsapi/api.php
@@ -7,7 +7,7 @@
 
         include ("dbconn.php");
     
-        $read = "SELECT album.number, album.title, artist.name, year.year, image.image FROM album
+        $read = "SELECT album.number, album.title, artist.name, year.year, genre.genre, subgenre.subgenre, image.image FROM album
         INNER JOIN artist
         ON album.artist_id = artist.id
         INNER JOIN year
@@ -15,7 +15,16 @@
         INNER JOIN album_image
         ON album.id = album_image.album_id
         INNER JOIN image
-        ON album_image.image_id = image.id";
+        ON album_image.image_id = image.id
+        INNER JOIN album_genre
+        ON album.id = album_genre.album_id
+        INNER JOIN genre
+        ON album_genre.genre_id = genre.id
+        INNER JOIN album_subgenre
+        ON album.id = album_subgenre.album_id
+        INNER JOIN subgenre
+        ON album_subgenre.subgenre_id = subgenre.id
+        ORDER BY album.number";
         
         $result = $conn->query($read);
         
