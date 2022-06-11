d46086c14a43b3ecf60e9dfcd0d3efb955a15167
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-06-11 23:25:58 +0100

    updated

--- qub/week00/albumswebsitenew/index.php
+++ qub/week00/albumswebsitenew/index.php
@@ -17,8 +17,7 @@ $data = json_decode($result, true);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Album Website</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" ></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" type="text/css" href="ui/styles.css">
