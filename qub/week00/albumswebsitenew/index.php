3579d41b9f4281f31d311206b08c4d20e82d7fe9
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-06-13 22:48:44 +0100

    updated

--- qub/week00/albumswebsitenew/index.php
+++ qub/week00/albumswebsitenew/index.php
@@ -14,41 +14,29 @@ $data = json_decode($result, true);
 <!DOCTYPE html>
 <html lang="en">
   <head>
+  <title>Album Website</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Album Website</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" type="text/css" href="ui/styles.css">
 </head>
 
 <body>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php">Record Website</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class='collapse navbar-collapse' id='navbarSupportedContent'>
-      <ul class='navbar-nav mr-auto'>
-        <?php
-        
-        ?>
-      </ul>
-
-      <form class='form-inline' action='search.php'>
-        <input class='form-control mr-sm-2' type='text' name='search' placeholder='Search'>
-        <button class='btn btn-success' type='submit'>Search</button>
-      </form>
-    </div>
-
-  </nav>
+<nav class="navbar bg-secondary navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand">Record Website</a>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
   <br>
 
   <div>
-    <h1>Top 10 Most Played Albums</h1>
+    <h1>Top 10 Most Listened To Albums</h1>
   </div>
 
   <div class="row row-cols-1 row-cols-md-5 g-4">
