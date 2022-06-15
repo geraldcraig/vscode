3d8517985268344e2d33a795fc87676348f619b8
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-06-15 18:26:15 +0100

    updated

--- qub/week00/albumswebsitenew/index.php
+++ qub/week00/albumswebsitenew/index.php
@@ -27,6 +27,9 @@ $data = json_decode($result, true);
 <nav class="navbar bg-dark navbar-dark">
   <div class="container-fluid">
     <a class="navbar-brand">Record Website</a>
+    <ul class="navbar-nav">
+      <a class="nav-link" href="albumlist.php">Top 500 Albums</a>
+    </ul>
     <form class="d-flex">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success" type="submit">Search</button>
