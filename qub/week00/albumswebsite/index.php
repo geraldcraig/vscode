842bcee46018200fca71a9394b1be7b2fae31588
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-06-17 19:01:51 +0100

    updated

--- qub/week00/albumswebsite/index.php
+++ qub/week00/albumswebsite/index.php
@@ -1,5 +1,13 @@
 <?php
 
+session_start();
+
+if (!isset($_SESSION['user'])) {
+  $showBtn = false;
+} else {
+  $showBtn = true;
+  $currentuser = $_SESSION['user'];
+}
 
 $endpoint = "http://localhost/qub/week00/albumsapi/api.php";
 
@@ -13,29 +21,29 @@ $data = json_decode($result, true);
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
   <title>Album Website</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="ui/styles.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="ui/styles.css">
 </head>
 
 <body>
 
-<nav class="navbar bg-dark navbar-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand">Record Website</a>
-    <ul class="navbar-nav">
-      <a class="nav-link" href="albumlist.php">Top 500 Albums</a>
-    </ul>
-    <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+  <nav class="navbar bg-dark navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand">Record Website</a>
+      <ul class="navbar-nav">
+        <a class="nav-link" href="albumlist.php">Top 500 Albums</a>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
   <br>
 
   <div>
@@ -69,6 +77,7 @@ $data = json_decode($result, true);
 
   </div>
 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 
 </html>
