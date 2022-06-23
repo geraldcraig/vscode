cbae34e54abdfdafab450c5115d6c9aa7c28eb5c
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-06-23 21:17:43 +0100

    updated

--- qub/week00/albumsapi/api.php
+++ qub/week00/albumsapi/api.php
@@ -3,7 +3,7 @@
     header("Content-Type: application/json");
 
     // display top 10 albums
-    if (($_SERVER['REQUEST_METHOD']==='GET') && (!isset($_GET['album']))) {
+    if (($_SERVER['REQUEST_METHOD']==='GET') && (!isset($_GET['album_list'])) && (!isset($_GET['album']))) {
 
         include ("dbconn.php");
     
@@ -50,7 +50,7 @@
     }
 
     // display all albums
-    if (($_SERVER['REQUEST_METHOD']==='GET') && (isset($_GET['album']))) {
+    if (($_SERVER['REQUEST_METHOD']==='GET') && (isset($_GET['album_list'])) && (!isset($_GET['album']))) {
 
         include ("dbconn.php");
     
@@ -72,6 +72,54 @@
         INNER JOIN subgenre
         ON album_subgenre.subgenre_id = subgenre.id
         ORDER BY album.number";
+        
+        $result = $conn->query($read);
+        
+        if (!$result) {
+            echo $conn -> error;
+        }
+    
+        // build a response array
+        $api_response = array();
+        
+        while ($row = $result->fetch_assoc()) {
+            
+            array_push($api_response, $row);
+        }
+            
+        // encode the response as JSON
+        $response = json_encode($api_response);
+        
+        // echo out the response
+        echo $response;
+
+    }
+
+     // get album
+     if (($_SERVER['REQUEST_METHOD']==='GET') && (!isset($_GET['album_list'])) && (isset($_GET['album']))) {
+
+        include ("dbconn.php");
+
+        $itemid = $conn->real_escape_string($_GET['album']);
+    
+        $read = "SELECT album.id, album.number, album.title, artist.name, year.year, genre.genre, subgenre.subgenre, image.image FROM album
+        INNER JOIN artist
+        ON album.artist_id = artist.id
+        INNER JOIN year
+        ON album.year_id = year.id
+        INNER JOIN album_image
+        ON album.id = album_image.album_id
+        INNER JOIN image
+        ON album_image.image_id = image.id
+        INNER JOIN album_genre
+        ON album.id = album_genre.album_id
+        INNER JOIN genre
+        ON album_genre.genre_id = genre.id
+        INNER JOIN album_subgenre
+        ON album.id = album_subgenre.album_id
+        INNER JOIN subgenre
+        ON album_subgenre.subgenre_id = subgenre.id
+        WHERE album.number = $itemid";
         
         $result = $conn->query($read);
         
