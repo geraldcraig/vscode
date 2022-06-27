4a8bc06f8a9a4b894d23342c96a57466d1582648
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-06-27 21:47:44 +0100

    admin added

--- qub/week00/albumswebsite/adminlogin.php
+++ qub/week00/albumswebsite/adminlogin.php
@@ -1,81 +1,41 @@
-<?php
-
-session_start();
-
-if (!isset($_SESSION['user'])) {
-  $showBtn =false;
-} else {
-  $showBtn = true;
-  $currentUser = $_SESSION['user'];
-}
-
-//$endpoint = "http://localhost/qub/week00/albumsapi/api.php";
-
-$endpoint = "http://gcraig15.webhosting6.eeecs.qub.ac.uk/albumsapi/api.php";
-
-$result = file_get_contents($endpoint);
-
-$data = json_decode($result, true);
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Record Website</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="ui/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-all.min.css">
+    <link rel="stylesheet" href="solar.css">
 </head>
 
-<body>
+<body class=" h-vh-100 bg-dark">
+    <div data-role="appbar" data-expand-point="md" class="bg-crimson fg-gray">
+        <a href="#" class="brand no-hover">
+            <span class="mif-spinner4 ani-spin black"> </span>  <span class="pl-2" >Solar Me</span>
+        </a>
 
-<nav class="navbar navbar-expand-lg bg-secondary navbar-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Record Website</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="albumlist.php">Top 500 Albums</a>
-        </li>
-        <?php
-        if (!$showBtn) {
-          echo "<li class='nav-item'>
-                  <a class='nav-link' href='login.php'>Log In</a>
-                </li>
-                <li class='nav-item'>
-                  <a class='nav-link' href='register.php'>Register</a>
-                </li>
-                <li class='nav-item'>
-                  <a class='nav-link' href='adminlogin.php'>Admin</a>
-                </li>";
-        } else {
-          echo "<li class='nav-item'>
-                  <a class='nav-link' href='account.php'>Account</a>
-                </li>
-                <li class='nav-item'>
-                  <a class='nav-link' href='logout.php'>Log Out</a>
-                </li>";
-        }
-        ?>
-       
-      </ul>
-      <form class="d-flex" action="search.php" method="get">
-        <input class="form-control me-2" type="text" name="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-primary" type="submit">Search</button>
-      </form>
+        <ul class="app-bar-menu">
+            <li><a href="index.php">Home</a></li>
+        </ul>
     </div>
-  </div>
-</nav>
 
-<div class="container-fluid mt-3">
-    <h1>Admin Login</h1>
-</div>
+    <div class="begincontent fg-white bg-black p-6 mx-auto border bd-default win-shadow">
+        <?php
+            echo "<div><h2>Login</h2></div>
+                <form method='POST' action='processlogin.php'>
+                <div class='form-group'>
+                <label>Username</label>
+                <input name='uname' type='text' class='metro-input' required='required' placeholder='Enter your username'>
+                </div>
+                <div class='form-group'>
+                <label>Password</label>
+                <input name='pword' type='password' class='metro-input' required='required' placeholder='Enter your password'>
+                </div>
+                <input class='button yellow outline pl-10 pr-10 mt-10 place-right' type='submit' value='login'>
+                </form>";     
+        ?>
+    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdn.metroui.org.ua/v4.3.2/js/metro.min.js"></script>
+
+</body>
 </html>
