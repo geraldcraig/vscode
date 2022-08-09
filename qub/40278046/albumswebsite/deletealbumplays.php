628b250954d01ebac245d144d927546fa7de5504
geraldcraig <geraldcraig@me.com>
2022-08-09 15:52:54 +0100

    updated

---
+++ qub/40278046/albumswebsite/deletealbumplays.php
@@ -0,0 +1,41 @@
+<?php
+
+$currentUser = $_GET['user_name'];
+$albumid = $_GET['album_id'];
+
+//$endpoint = "http://localhost/qub/40278046/albumsapi/api.php?deletealbumplays";
+
+$endpoint = "http://gcraig15.webhosting6.eeecs.qub.ac.uk/albumsapi/api.php?deletealbumplays";
+
+$postdata = http_build_query(
+    array(
+        'deleteuser' => $currentUser,
+        'deletealbumid' => $albumid
+      )
+  );
+
+$opts = array(
+
+  'http' => array(
+    'method' => 'DELETE',
+    'header' => 'Content-Type: application/x-www-form-urlencoded',
+    'content' => $postdata
+  )
+
+);
+
+$context = stream_context_create($opts);
+$resource = file_get_contents($endpoint, false, $context);
+
+echo $resource;
+
+  if ($resource != FALSE) {
+
+    header("Location: accountalbumplays.php");
+    
+  } else {
+
+    echo "Unable to delete album plays";
+  }
+?>
+
