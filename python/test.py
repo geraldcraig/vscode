bb29ca0098043f6f190dc4fdc9fb663eb0f418ce
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-08-25 17:01:46 +0100

    python updated

--- python/test.py
+++ python/test.py
@@ -1,11 +1,24 @@
-try:
-    value = input("value")
-    print(int(value)/len(value))
-except ValueError:
-    print("bad")
-except ZeroDivisionError:
-    print("very bad")
-except TypeError:
-    print("very very")
-except:
-    print("boo")
+class I:
+    def __init__(self):
+        self.s = 'abc'
+        self.i = 0
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if self.i == len(self.s):
+                raise StopIteration
+        v = self.s[self.i]
+        self.i += 1
+        return v
+
+for x in I():
+    print(x, end='')            
+
+
+
+
+
+
+
