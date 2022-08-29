d3ab8215302564d29fd4a5301f7c201669cd92e3
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-08-29 17:01:46 +0100

    python updated

--- python/test.py
+++ python/test.py
@@ -1,14 +1,7 @@
-from os import strerror
+from datetime import datetime
 
-try:
-	fo = open('newtext.txt', 'wt') # A new file (newtext.txt) is created.
-	for i in range(10):
-		s = "line #" + str(i+1) + "\n"
-		for ch in s:
-			fo.write(ch)
-	fo.close()
-except IOError as e:
-	print("I/O error occurred: ", strerror(e.errno))       
+datetime = datetime(2019, 11, 27, 11, 27,22)
+print(datetime.strftime('%Y/%m/%d %H:%M:%S'))
 
 
 
