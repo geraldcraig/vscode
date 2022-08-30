8f3aea78528894cb3394871aba09e5723588fcc2
geraldcraig <geraldcraig@me.com>
2022-08-30 15:19:22 +0100

    project updated

---
+++ fit/day08/exercise01.js
@@ -0,0 +1,9 @@
+var now = new Date();
+var dayOfWeek = now.getDay();
+
+if (dayOfWeek == 5) {
+    console.log("Have a nice weekend!")
+}
+
+
+
---
+++ fit/project/scripts/script.js
@@ -0,0 +1,17 @@
+// jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
+
+document.getElementById("btnLoad").addEventListener("click", function()
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "someText.txt", true);
+    xhttp.send();
+});
