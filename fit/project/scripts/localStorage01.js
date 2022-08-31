a6735b68c2251a0a2aaa6e5845638459fc7e4541
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-08-31 21:52:44 +0100

    updated

--- fit/project/scripts/localStorage01.js
+++ fit/project/scripts/localStorage01.js
@@ -33,13 +33,15 @@ document.getElementById("btnSet").addEventListener("click", function()
     var data1 = document.getElementById("setData1");
     var data2 = document.getElementById("setData2");
     var data3 = document.getElementById("setData3");
+    var comment = document.getElementById("comment");
     var userInput1 = data1.value;
     var userInput2 = data2.value;
     var userInput3 = data3.value;
+    var userInput4 = comment.value;
     localStorage.setItem("userData1", userInput1);
     localStorage.setItem("userData2", userInput2);
     localStorage.setItem("userData3", userInput3);
-
+    localStorage.setItem("comment", userInput4);
 });
 
 document.getElementById("btnGet").addEventListener("click", function()
@@ -47,6 +49,10 @@ document.getElementById("btnGet").addEventListener("click", function()
     var data1 = localStorage.getItem("userData1");
     var data2 = localStorage.getItem("userData2");
     var data3 = localStorage.getItem("userData3");
-    document.getElementById("result").innerHTML = "The value stored in localStorage for 'userData' is: " + "\nFirst Name: " + data1 + "\nLast Name:  " + data2 + "\nEmail Address: " + data3;
-    
+    var comment = localStorage.getItem("comment");
+    document.getElementById("result").innerHTML = "The value stored in localStorage for 'userData' is: ";
+    document.getElementById("result1").innerHTML = "First Name: " + data1;
+    document.getElementById("result2").innerHTML = "Last Name:  " + data2;
+    document.getElementById("result3").innerHTML = "Email Address: " + data3;
+    document.getElementById("result4").innerHTML = "Comment: " + comment;
 });
