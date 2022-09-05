170f4fd7443cd83ab7f14970b33a08aacd455498
geraldcraig <geraldcraig@me.com>
2022-09-05 15:01:42 +0100

    updated

--- fit/project/scripts/script.js
+++ fit/project/scripts/script.js
@@ -1,10 +1,9 @@
-// jQuery for hero image to consume the header window space
-$(document).ready(function(){
+/* jQuery for hero image to consume the header window space
+$(document).ready(function () {
     $('.hero').height($(window).height());
-});
+}); */
 
-document.getElementById("btnLoad").addEventListener("click", function()
-{
+document.getElementById("btnLoad").addEventListener("click", function () {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -15,4 +14,19 @@ document.getElementById("btnLoad").addEventListener("click", function()
     xhttp.open("GET", "specialOffers.txt", true);
     xhttp.send();
 });
+
+/* x = document.getElementById("demo5");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "geolocation is not supported by this browser"
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}*/
 
