8230dfb20c0a74dc933dedb26b1635af49dff2a8
geraldcraig <geraldcraig@me.com>
2022-09-06 15:12:41 +0100

    updated

---
+++ fit/project/scripts/canvas-demo.js
@@ -0,0 +1,10 @@
+// canvas-demo.js
+
+const demoCanvas = document.getElementById(’canvas-demo’).getContext(’2d’);
+
+window.onload = function() {// make sure to use onload
+    
+/* Add code here as we go!!! @nodocs
+ */
+    
+}
--- fit/project/scripts/localStorage01.js
+++ fit/project/scripts/localStorage01.js
@@ -1,9 +1,9 @@
 /* Create a function to validate the form */
 function validation() {
     /* Create variable to store the values from the form */
-    var fname = document.getElementById("setData1").value;
-    var lname = document.getElementById("setData2").value;
-    var email = document.getElementById("setData3").value;
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var email = document.getElementById("email").value;
     var comment = document.getElementById("comment").value;
 
     /* */
@@ -35,29 +35,29 @@ function validation() {
 
 document.getElementById("btnSet").addEventListener("click", function()
 {
-    var data1 = document.getElementById("setData1");
-    var data2 = document.getElementById("setData2");
-    var data3 = document.getElementById("setData3");
+    var fname = document.getElementById("fname");
+    var lname = document.getElementById("lname");
+    var email = document.getElementById("email");
     var comment = document.getElementById("comment");
-    var userInput1 = data1.value;
-    var userInput2 = data2.value;
-    var userInput3 = data3.value;
+    var userInput1 = fname.value;
+    var userInput2 = lname.value;
+    var userInput3 = email.value;
     var userInput4 = comment.value;
-    localStorage.setItem("userData1", userInput1);
-    localStorage.setItem("userData2", userInput2);
-    localStorage.setItem("userData3", userInput3);
+    localStorage.setItem("fname", userInput1);
+    localStorage.setItem("lname", userInput2);
+    localStorage.setItem("email", userInput3);
     localStorage.setItem("comment", userInput4);
 });
 
 document.getElementById("btnGet").addEventListener("click", function()
 {
-    var data1 = localStorage.getItem("userData1");
-    var data2 = localStorage.getItem("userData2");
-    var data3 = localStorage.getItem("userData3");
+    var fname = localStorage.getItem("fname");
+    var lname = localStorage.getItem("lname");
+    var email = localStorage.getItem("email");
     var comment = localStorage.getItem("comment");
     document.getElementById("result").innerHTML = "The data entered into the Contact Us form is: ";
-    document.getElementById("result1").innerHTML = "First Name: " + data1;
-    document.getElementById("result2").innerHTML = "Last Name:  " + data2;
-    document.getElementById("result3").innerHTML = "Email Address: " + data3;
+    document.getElementById("result1").innerHTML = "First Name: " + fname;
+    document.getElementById("result2").innerHTML = "Last Name:  " + lname;
+    document.getElementById("result3").innerHTML = "Email Address: " + email;
     document.getElementById("result4").innerHTML = "Comment: " + comment;
 });
