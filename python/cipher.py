c97566fad4b90c932b65171bdce18d81245be3a0
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-09-08 16:44:39 +0100

    python updated

--- python/cipher.py
+++ python/cipher.py
@@ -1,10 +1,11 @@
 text = input("Enter word to cipher: ")
+num = int(input("enter birth month: "))
 cipher = ''
 for char in text:
     if not char.isalpha():
         continue
     char = char.upper()
-    code = ord(char) + 1
+    code = ord(char) + num
     if code > ord('Z'):
         code = ord('A')
     cipher += chr(code)
