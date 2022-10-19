dabaeb050b6dc15f114d8383b5575f41ac352f16
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-10-19 17:39:05 +0100

    music player updated

---
+++ electron/music-player/player.js
@@ -0,0 +1,11 @@
+Amplitude.init({
+    songs: [
+        {
+            name: 'Moment of Truth',
+            artist: 'David Hilowitz',
+            album: 'Moment of Truth',
+            url: './music/David_Hilowitz_Moment_of_Truth.mp3',
+            cover_art_url: './music/hilowitz.jpeg'
+        }
+    ]
+});
